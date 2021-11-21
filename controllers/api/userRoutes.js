4f01e5a37d66257cbfc0b7ce73fc9924057bd579
theDaveB <davebloisesquire@gmail.com>
2021-11-20 19:18:49 -0800

    Adding additional user routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,36 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+// GET all users
+router.get('/', (req, res) => {
+  User.findAll()
+  .then((users) => {
+    res.json(users);
+  })
+  .catch((error) => res.json(error))
+})
+
+// POST new user
+router.post('/', async (req, res) => {
+  try {
+    const userData = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    req.session.save(() => {
+      req.session.loggedIn = true;
+req.session.user = userData
+      res.status(200).json(userData);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// LOGIN for existing USER
 router.post('/login', async (req, res) => {
   try {
     // Find the user who matches the posted e-mail address
@@ -36,6 +66,7 @@ router.post('/login', async (req, res) => {
   }
 });
 
+// LOGOUT of CURRENT SESSION
 router.post('/logout', (req, res) => {
   if (req.session.logged_in) {
     // Remove the session variables
