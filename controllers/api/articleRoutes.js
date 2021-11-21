8342c98e818968838f8c461c9081c07c60b84304
theDaveB <davebloisesquire@gmail.com>
2021-11-20 18:28:17 -0800

    Models built (for the most part) and controllers well underway

---
+++ controllers/api/articleRoutes.js
@@ -0,0 +1,47 @@
+const router = require('express').Router();
+const { Article } = require('../../models');
+
+// Get all articles
+router.get('/', (req, res) => {
+  Article.findAll({
+    include: { model: "User" }
+  })
+  .then((allArticles) => res.json(allArticles))
+  .catch((error) => res.json(error))
+})
+
+// Get one article by ID
+router.get('/:id', (req, res) => {
+  Article.findByPk(req.params.id, {
+    include: [
+      { model: "User" },
+      { model: "Comment" }
+    ]
+  })
+  .then((articleById) => res.json(articleById))
+  .catch((error) => res.json(error))
+})
+
+// Get Articles by User_id
+router.get('/user/:user_id', (req, res) => {
+  Article.findAll({
+    where: {
+      user_id: req.params.user_id
+    }
+  })
+})
+
+// Post new Article
+router.post('/', (req, res) => {
+
+})
+
+// Update exisiting Article
+router.put('/:id', (req, res) => {
+
+})
+
+// Delete Article
+router.delete('/:id', (req, res) => {
+
+})
---
+++ models/Comment.js
@@ -0,0 +1,43 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    Comment: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    article_id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      references: {
+        model: "Article",
+        key: "id"
+      }
+    },
+    user_id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      references: {
+        model: "User",
+        key: "id"
+      }
+    }
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment',
+  }
+);
+
+module.exports = Comment;
